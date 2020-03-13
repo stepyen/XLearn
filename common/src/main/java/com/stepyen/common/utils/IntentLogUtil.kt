ad682d81b96b57994bf57dd6b052b2ea53f99c40
stepyen <stepyen@163.com>
2020-03-13 16:32:07 +0800

    【增加】 1、优化日志打印类

--- common/src/main/java/com/stepyen/common/utils/IntentLogUtil.kt
+++ common/src/main/java/com/stepyen/common/utils/IntentLogUtil.kt
@@ -62,23 +62,50 @@ object IntentLogUtil {
             }
 
             flags?.let {
-                sb.append("flags: ${it.toString()}\n")
-                val flag = when (it) {
-                    0->"无"
-                    32768-> "FLAG_ACTIVITY_CLEAR_TASK"      // 0X00008000
-                    268435456->"FLAG_ACTIVITY_NEW_TASK"         // 10000000
-                    270532608->"FLAG_ACTIVITY_NEW_TASK & FLAG_ACTIVITY_FORWARD_RESULT"         // 10200000
-                    272629760 ->"FLAG_ACTIVITY_NEW_TASK & FLAG_ACTIVITY_CLEAR_TOP"         // 10400000
-                    else->"无"
-                }
-                sb.append("$flag\n")
-                    .append("\n")
+                sb.append("flags: ${getFlagStr(it)}\n")
             }
 
 
         }
 
         return sb.toString()
+
+    }
+
+
+    private fun getFlagStr(flags:Int) :String{
+
+        val flagMap = mutableMapOf(
+                Intent.FLAG_ACTIVITY_CLEAR_TASK to "FLAG_ACTIVITY_CLEAR_TASK",
+                Intent.FLAG_ACTIVITY_NEW_TASK to "FLAG_ACTIVITY_NEW_TASK",
+                Intent.FLAG_ACTIVITY_SINGLE_TOP to "FLAG_ACTIVITY_SINGLE_TOP",
+                Intent.FLAG_ACTIVITY_CLEAR_TOP to "FLAG_ACTIVITY_CLEAR_TOP",
+                Intent.FLAG_ACTIVITY_FORWARD_RESULT to "FLAG_ACTIVITY_FORWARD_RESULT",
+                0x00400000 to "FLAG_RECEIVER_FROM_SHELL"
+        )
+
+        var tempFlags = 0
+
+        val sb =StringBuilder().apply {
+
+            for (flag in flagMap) {
+                if (flags and flag.key == flag.key) {
+                    append("${flag.value} or ")
+                    tempFlags += flag.key
+                }
+            }
+
+        }
+
+        if (tempFlags == flags) {
+            sb.append("\n 完全匹配")
+        }else{
+            sb.append("\n 不完全匹配 flag：$flags 当前匹配总值：$tempFlags 差值：${flags - tempFlags}")
+        }
+
+
+        return sb.append("\n\n").toString()
+
 
     }
 }
