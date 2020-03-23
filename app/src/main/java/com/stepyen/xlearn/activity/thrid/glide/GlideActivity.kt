54b35cdda0c85bac2ec2a7dd8232482b35a4d943
stepyen <stepyen@163.com>
2020-03-23 18:29:16 +0800

    【增加】 1、glide

---
+++ app/src/main/java/com/stepyen/xlearn/activity/thrid/glide/GlideActivity.kt
@@ -0,0 +1,28 @@
+package com.stepyen.xlearn.activity.thrid.glide
+
+import android.util.Log
+import com.stepyen.xlearn.App
+import com.stepyen.xlearn.R
+import com.stepyen.xlearn.base.BasePageActivity
+import kotlinx.android.synthetic.main.activity_msa.*
+
+/**
+ * Glide
+ *
+ *
+ *
+ */
+class GlideActivity : BasePageActivity() {
+
+
+    companion object{
+        const val TAG = "GlideTAG"
+    }
+    override fun initView() {
+        addView(R.layout.activity_glide)
+
+
+    }
+
+
+}
